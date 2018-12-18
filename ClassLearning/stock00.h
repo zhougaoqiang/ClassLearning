580e75c0b08932a84b78f6f129ca9e732be44937
zhougaoqiang <zhougaoqiang1991@gmail.com>
2018-12-18 23:20:53 +0800

    First Class learning

---
+++ ClassLearning/stock00.h
@@ -0,0 +1,28 @@
+//stock00.h --stock class interface
+//version 00
+
+#ifndef STOCK00_H_
+#define STOCK00_H_
+
+#include <string>
+#include "pch.h"
+
+class Stock //class declaration
+{
+private:
+	std::string company;
+	long shares;
+	double share_val;
+	double total_val;
+	void set_tot() { total_val = shares * share_val; }
+public:
+	Stock(); //default constructor
+	Stock(const std::string &co, long n, double pr);
+	~Stock(); //noisy constructor
+	void buy(long num, double price);
+	void sell(long num, double price);
+	void update(double price);
+	void show();
+};    //note semicolon at the end
+
+#endif
